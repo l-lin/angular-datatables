a745c169e7514ab6df82772afb040586f784c8bb
Louis LIN <louis.lin.87@gmail.com>
2014-06-16 18:18:53 +0200

    Watch promise changes (feature #15) + Add a RendererFactory for better readability

--- dist/angular-datatables.js
+++ dist/angular-datatables.js
@@ -210,11 +210,10 @@
     sAjaxDataProp: '',
     aoColumns: []
   }).directive('datatable', [
-    '$http',
     'DT_DEFAULT_OPTIONS',
     '$timeout',
     'DT_LAST_ROW_KEY',
-    function ($http, DT_DEFAULT_OPTIONS, $timeout, DT_LAST_ROW_KEY) {
+    function (DT_DEFAULT_OPTIONS, $timeout, DT_LAST_ROW_KEY) {
       var $loading = angular.element('<h3>Loading...</h3>');
       var _doRenderDataTable = function ($elem, options) {
         // Add $timeout to be sure that angular has finished rendering before calling datatables
@@ -222,13 +221,93 @@
           // Show datatable & hide loading
           $elem.show();
           $loading.hide();
+          // Condition to refresh the dataTable
           $elem.dataTable(options);
         }, 0, false);
       };
-      var _renderDataTableIfNoNgRows = function ($elem, isNgDisplay, options) {
-        if (!isNgDisplay) {
-          _doRenderDataTable($elem, options);
-        }
+      var RendererFactory = {
+          fromOptions: function (options, isNgDisplay) {
+            if (isNgDisplay) {
+              return new NGRenderer(options);
+            }
+            if (angular.isDefined(options)) {
+              if (angular.isObject(options.dataPromise)) {
+                return new PromiseDTRenderer(options);
+              }
+              if (angular.isDefined(options.sAjaxDataProp)) {
+                return new AjaxRenderer(options);
+              }
+              return new DefaultRenderer(options);
+            }
+            return new DefaultRenderer();
+          }
+        };
+      /**
+         * Default renderer without any server call
+         * @constructor
+         */
+      var DefaultRenderer = function (options) {
+        var renderer = { options: options };
+        renderer.render = function ($scope, $elem) {
+          _doRenderDataTable($elem, renderer.options);
+        };
+        return renderer;
+      };
+      var NGRenderer = function (options) {
+        var renderer = new DefaultRenderer(options);
+        renderer.render = function ($scope, $elem) {
+          $scope.$on(DT_LAST_ROW_KEY, function () {
+            _doRenderDataTable($elem, renderer.options);
+          });
+        };
+        return renderer;
+      };
+      var PromiseDTRenderer = function (options) {
+        var renderer = { options: options }, oTable;
+        renderer.render = function ($scope, $elem) {
+          // Watch changes
+          $scope.$watch('dtOptions.dataPromise', function (promise) {
+            promise.then(function (data) {
+              options.aaData = data;
+              // Add $timeout to be sure that angular has finished rendering before calling datatables
+              $timeout(function () {
+                // Show datatable & hide loading
+                $elem.show();
+                $loading.hide();
+                // Set it to true in order to be able to redraw the dataTable
+                options.bDestroy = true;
+                // Condition to refresh the dataTable
+                if (oTable) {
+                  oTable.fnClearTable();
+                  oTable.fnDraw();
+                  oTable.fnAddData(options.aaData);
+                } else {
+                  oTable = $elem.dataTable(options);
+                }
+              }, 0, false);
+            });
+          });
+        };
+        return renderer;
+      };
+      var AjaxRenderer = function (options) {
+        var renderer = { options: options };
+        renderer.render = function ($scope, $elem) {
+          // Define default values in case it is an ajax datatables
+          if (angular.isUndefined(options.sAjaxDataProp)) {
+            options.sAjaxDataProp = DT_DEFAULT_OPTIONS.sAjaxDataProp;
+          }
+          if (angular.isUndefined(options.aoColumns)) {
+            options.aoColumns = DT_DEFAULT_OPTIONS.aoColumns;
+          }
+          $scope.$watch('dtOptions.sAjaxSource', function (sAjaxSource) {
+            options.sAjaxSource = sAjaxSource;
+            // Set it to true in order to be able to redraw the dataTable
+            options.bDestroy = true;
+            _doRenderDataTable($elem, renderer.options);
+          });
+        };
+        return renderer;
       };
       return {
         restrict: 'A',
@@ -241,6 +320,7 @@
           // Display loading
           $elem.after($loading);
           $elem.hide();
+          // Build options
           var isNgDisplay = $scope.datatable && $scope.datatable === 'ng', options;
           if (angular.isDefined($scope.dtOptions)) {
             options = {};
@@ -249,29 +329,9 @@
             if (angular.isArray($scope.dtColumns)) {
               options.aoColumns = $scope.dtColumns;
             }
-            if (angular.isObject(options.dataPromise)) {
-              options.dataPromise.then(function (data) {
-                options.aaData = data;
-                _renderDataTableIfNoNgRows($elem, isNgDisplay, options);
-              });
-            } else {
-              if (angular.isDefined(options.sAjaxSource)) {
-                // Define default values in case it is an ajax datatables
-                if (angular.isUndefined(options.sAjaxDataProp)) {
-                  options.sAjaxDataProp = DT_DEFAULT_OPTIONS.sAjaxDataProp;
-                }
-                if (angular.isUndefined(options.aoColumns)) {
-                  options.aoColumns = DT_DEFAULT_OPTIONS.aoColumns;
-                }
-              }
-              _renderDataTableIfNoNgRows($elem, isNgDisplay, options);
-            }
-          } else {
-            _renderDataTableIfNoNgRows($elem, isNgDisplay);
           }
-          $scope.$on(DT_LAST_ROW_KEY, function () {
-            _doRenderDataTable($elem, options);
-          });
+          // Render dataTable
+          RendererFactory.fromOptions(options, isNgDisplay).render($scope, $elem);
         }
       };
     }
--- src/angular-datatables.directive.js
+++ src/angular-datatables.directive.js
@@ -6,7 +6,7 @@
         sAjaxDataProp: '',
         aoColumns: []
     }).
-    directive('datatable', function($http, DT_DEFAULT_OPTIONS, $timeout, DT_LAST_ROW_KEY) {
+    directive('datatable', function(DT_DEFAULT_OPTIONS, $timeout, DT_LAST_ROW_KEY) {
         var $loading = angular.element('<h3>Loading...</h3>');
         var _doRenderDataTable = function($elem, options) {
             // Add $timeout to be sure that angular has finished rendering before calling datatables
@@ -14,14 +14,106 @@
                 // Show datatable & hide loading
                 $elem.show();
                 $loading.hide();
+                // Condition to refresh the dataTable
                 $elem.dataTable(options);
             }, 0, false);
         };
-        var _renderDataTableIfNoNgRows = function ($elem, isNgDisplay, options) {
-            if (!isNgDisplay) {
-                _doRenderDataTable($elem, options);
+        var RendererFactory = {
+            fromOptions: function(options, isNgDisplay) {
+                if (isNgDisplay) {
+                    return new NGRenderer(options);
+                }
+                if (angular.isDefined(options)) {
+                    if (angular.isObject(options.dataPromise)) {
+                        return new PromiseDTRenderer(options);
+                    }
+                    if (angular.isDefined(options.sAjaxDataProp)) {
+                        return new AjaxRenderer(options);
+                    }
+                    return new DefaultRenderer(options);
+                }
+                return new DefaultRenderer();
             }
         };
+
+        /**
+         * Default renderer without any server call
+         * @constructor
+         */
+        var DefaultRenderer = function(options) {
+            var renderer = {
+                options: options
+            };
+            renderer.render = function($scope, $elem) {
+                _doRenderDataTable($elem, renderer.options);
+            };
+            return renderer;
+        };
+
+        var NGRenderer = function(options) {
+            var renderer = new DefaultRenderer(options);
+            renderer.render = function ($scope, $elem) {
+                $scope.$on(DT_LAST_ROW_KEY, function () {
+                    _doRenderDataTable($elem, renderer.options);
+                });
+            };
+            return renderer;
+        };
+
+        var PromiseDTRenderer = function(options) {
+            var renderer = {
+                options: options
+            }, oTable;
+            renderer.render = function($scope, $elem) {
+                // Watch changes
+                $scope.$watch('dtOptions.dataPromise', function(promise) {
+                    promise.then(function(data) {
+                        options.aaData = data;
+                        // Add $timeout to be sure that angular has finished rendering before calling datatables
+                        $timeout(function() {
+                            // Show datatable & hide loading
+                            $elem.show();
+                            $loading.hide();
+                            // Set it to true in order to be able to redraw the dataTable
+                            options.bDestroy = true;
+                            // Condition to refresh the dataTable
+                            if (oTable) {
+                                oTable.fnClearTable();
+                                oTable.fnDraw();
+                                oTable.fnAddData(options.aaData);
+                            } else {
+                                oTable = $elem.dataTable(options);
+                            }
+                        }, 0, false);
+                    });
+                });
+            };
+            return renderer;
+        };
+
+        var AjaxRenderer = function (options) {
+            var renderer = {
+                options: options
+            };
+            renderer.render = function($scope, $elem) {
+                // Define default values in case it is an ajax datatables
+                if (angular.isUndefined(options.sAjaxDataProp)) {
+                    options.sAjaxDataProp = DT_DEFAULT_OPTIONS.sAjaxDataProp;
+                }
+                if (angular.isUndefined(options.aoColumns)) {
+                    options.aoColumns = DT_DEFAULT_OPTIONS.aoColumns;
+                }
+                $scope.$watch('dtOptions.sAjaxSource', function(sAjaxSource) {
+                    options.sAjaxSource = sAjaxSource;
+                    // Set it to true in order to be able to redraw the dataTable
+                    options.bDestroy = true;
+                    _doRenderDataTable($elem, renderer.options);
+                });
+            };
+            return renderer;
+        };
+
+
         return {
             restrict: 'A',
             scope: {
@@ -34,6 +126,7 @@
                 $elem.after($loading);
                 $elem.hide();
 
+                // Build options
                 var isNgDisplay = $scope.datatable && $scope.datatable === 'ng',
                     options;
                 if (angular.isDefined($scope.dtOptions)) {
@@ -43,32 +136,9 @@
                     if (angular.isArray($scope.dtColumns)) {
                         options.aoColumns = $scope.dtColumns;
                     }
-
-                    if (angular.isObject(options.dataPromise)) {
-                        options.dataPromise.then(function(data) {
-                            options.aaData = data;
-                            _renderDataTableIfNoNgRows($elem, isNgDisplay, options);
-                        });
-                    } else {
-                        if (angular.isDefined(options.sAjaxSource)) {
-                            // Define default values in case it is an ajax datatables
-                            if (angular.isUndefined(options.sAjaxDataProp)) {
-                                options.sAjaxDataProp = DT_DEFAULT_OPTIONS.sAjaxDataProp;
-                            }
-                            if (angular.isUndefined(options.aoColumns)) {
-                                options.aoColumns = DT_DEFAULT_OPTIONS.aoColumns;
-                            }
-                        }
-
-                        _renderDataTableIfNoNgRows($elem, isNgDisplay, options);
-                    }
-                } else {
-                    _renderDataTableIfNoNgRows($elem, isNgDisplay);
                 }
-
-                $scope.$on(DT_LAST_ROW_KEY, function () {
-                    _doRenderDataTable($elem, options);
-                });
+                // Render dataTable
+                RendererFactory.fromOptions(options, isNgDisplay).render($scope, $elem);
             }
         };
     }).
