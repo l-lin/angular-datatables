56cfa7441f0d4ad1a87d19adb5d358cae7aad2d5
Louis LIN <louis.lin.87@gmail.com>
2014-07-18 11:20:05 +0200

    Correct angular way the options

--- demo/angularWayWithOptions.js
+++ demo/angularWayWithOptions.js
@@ -4,13 +4,8 @@
  */
 (function() {
     'use strict';
-    angular.module('datatablesSampleApp').controller('angularWayWithOptionsCtrl', function ($scope, $resource, DTOptionsBuilder, DTColumnBuilder) {
+    angular.module('datatablesSampleApp').controller('angularWayWithOptionsCtrl', function ($scope, $resource, DTOptionsBuilder) {
         $scope.persons = $resource('data.json').query();
         $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withDisplayLength(2);
-        $scope.dtColumns = [
-            DTColumnBuilder.newColumn('id').withTitle('ID'),
-            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-            DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-        ];
     });
 })();
