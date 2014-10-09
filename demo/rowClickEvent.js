5e127794c82580bc44cc9d29836464d131971235
Louis LIN <louis.lin.87@gmail.com>
2014-10-09 23:28:12 +0200

    Update doc for duplicate click handlers #87

--- demo/rowClickEvent.js
+++ demo/rowClickEvent.js
@@ -13,9 +13,12 @@
         $scope.dtOptions = DTOptionsBuilder.fromSource('data.json')
             .withPaginationType('full_numbers')
             .withOption('rowCallback', function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+                // Unbind first in order to avoid any duplicate handler (see https://github.com/l-lin/angular-datatables/issues/87)
+                $('td', nRow).unbind('click');
                 $('td', nRow).bind('click', function() {
                     $scope.$apply(function() {
                         $scope.someClickHandler(aData);
+                        console.log('foo')
                     });
                 });
                 return nRow;
