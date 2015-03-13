0f6db5d0cbc6aefa816e46b82bb9cddc9c611834
Louis LIN <louis.lin.87@gmail.com>
2015-03-13 10:08:54 +0100

    Improve doc #245

--- demo/advanced/rowSelect.js
+++ demo/advanced/rowSelect.js
@@ -7,21 +7,20 @@ function RowSelect($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilde
     vm.selected = {};
     vm.selectAll = false;
     vm.toggleAll = toggleAll;
-    vm.toggleOne = toggleOne;
 
     var titleHtml = '<input type="checkbox" ng-model="showCase.selectAll"' +
         'ng-click="showCase.toggleAll(showCase.selectAll, showCase.selected)">';
 
     vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-        return $resource('data1.json').query().$promise;
-    })
+            return $resource('data1.json').query().$promise;
+        })
         .withOption('createdRow', function(row, data, dataIndex) {
             // Recompiling so we can bind Angular directive to the DT
             $compile(angular.element(row).contents())($scope);
         })
         .withOption('headerCallback', function(header) {
             if (!$scope.headerCompiled) {
-                //use this headerCompiled field to only compile header once
+                // Use this headerCompiled field to only compile header once
                 $scope.headerCompiled = true;
                 $compile(angular.element(header).contents())($scope);
             }
@@ -31,8 +30,7 @@ function RowSelect($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilde
         DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
             .renderWith(function(data, type, full, meta) {
                 vm.selected[full.id] = false;
-                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']"' +
-                    'ng-click="showCase.toggleOne(showCase.selected)"/>';
+                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']"/>';
             }),
         DTColumnBuilder.newColumn('id').withTitle('ID'),
         DTColumnBuilder.newColumn('firstName').withTitle('First name'),
@@ -51,18 +49,5 @@ function RowSelect($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilde
                 selectedItems[id] = selectAll;
             }
         }
-    };
-
-    function toggleOne (selectedItems) {
-        var me = this;
-        for (var id in selectedItems) {
-            if (selectedItems.hasOwnProperty(id)) {
-                if(!selectedItems[id]) {
-                    me.selectAll = false;
-                    return;
-                }
-            }
-        }
-        me.selectAll = true;
-    };
+    }
 }
