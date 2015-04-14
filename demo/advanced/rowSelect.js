43115b9612f7e970f7f77e62482a4dac5b22bc83
Louis LIN <louis.lin.87@gmail.com>
2015-04-14 17:19:03 +0200

    Correct rowSelect example #274

--- demo/advanced/rowSelect.js
+++ demo/advanced/rowSelect.js
@@ -31,8 +31,7 @@ function RowSelect($compile, $scope, $resource, DTOptionsBuilder, DTColumnBuilde
         DTColumnBuilder.newColumn(null).withTitle(titleHtml).notSortable()
             .renderWith(function(data, type, full, meta) {
                 vm.selected[full.id] = false;
-                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']"' +
-                    'ng-click="showCase.toggleOne(showCase.selected)"/>';
+                return '<input type="checkbox" ng-model="showCase.selected[' + data.id + ']" ng-click="showCase.toggleOne(showCase.selected)"/>';
             }),
         DTColumnBuilder.newColumn('id').withTitle('ID'),
         DTColumnBuilder.newColumn('firstName').withTitle('First name'),
