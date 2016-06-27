110ffdbba4aec02c2fe5ba5d8fc35edd872f6c36
Louis LIN <louis.lin.87@gmail.com>
2016-06-27 15:58:43 +0200

    Use  instead of  in doc #777

--- demo/basic/withPromise.js
+++ demo/basic/withPromise.js
@@ -1,10 +1,14 @@
 'use strict';
 angular.module('showcase.withPromise', ['datatables', 'ngResource']).controller('WithPromiseCtrl', WithPromiseCtrl);
 
-function WithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $resource) {
+function WithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $http, $q) {
     var vm = this;
     vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-        return $resource('data.json').query().$promise;
+        var defer = $q.defer();
+        $http.get('data.json').then(function(result) {
+            defer.resolve(result.data);
+        });
+        return defer.promise;
     }).withPaginationType('full_numbers');
 
     vm.dtColumns = [
