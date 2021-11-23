cf8020825439cae42dffe87392d2c755c12c03f5
Surya Teja K <shanmukhateja@gmail.com>
2021-11-23 10:52:54 +0530

    feat: add BS5 support for ng add

--- schematics/src/ng-add/models/style-options.ts
+++ schematics/src/ng-add/models/style-options.ts
@@ -2,6 +2,7 @@ export enum ADTStyleOptions {
   DT="dt",
   BS3="bs3",
   BS4="bs4",
+  BS5="bs5",
 }
 
 export const ADT_SUPPORTED_STYLES = [
@@ -32,6 +33,16 @@ export const ADT_SUPPORTED_STYLES = [
     angularJson: [
       { path: 'node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css', target: 'styles', fancyName: 'DataTables.net Bootstrap 4 CSS' },
       { path: 'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', target: 'scripts', fancyName: 'DataTables.net Bootstrap 4 JS' },
+    ]
+  },
+  {
+    style: ADTStyleOptions.BS5,
+    packageJson: [
+      { version: '^1.11.3', name: 'datatables.net-bs5', isDev: false },
+    ],
+    angularJson: [
+      { path: 'node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css', target: 'styles', fancyName: 'DataTables.net Bootstrap 5 CSS' },
+      { path: 'node_modules/datatables.net-bs5/js/dataTables.bootstrap5.min.js', target: 'scripts', fancyName: 'DataTables.net Bootstrap 5 JS' },
     ]
   },
 ]
