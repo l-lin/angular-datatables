1d35323dc0d6067f95853a78a4c9a236eaba07fb
Surya Teja K <shanmukhateja@gmail.com>
2022-12-27 23:57:32 +0530

    feat: use Materialize Tabs

--- demo/src/app/base-demo/base-demo.component.ts
+++ demo/src/app/base-demo/base-demo.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+// needed to re-init tabs on route change
+declare var $: any;
 
 @Component({
   selector: 'app-base-demo',
   templateUrl: './base-demo.component.html',
   styleUrls: ['./base-demo.component.css']
 })
-export class BaseDemoComponent {
+export class BaseDemoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   @Input()
   pageTitle = '';
@@ -38,6 +45,15 @@ export class BaseDemoComponent {
 
   @Input()
   deprecated = false;
+
+  ngOnInit() {
+    // Re-init tabs on route change
+    this.router.events
+    .pipe(filter(_ => _ instanceof NavigationEnd))
+    .subscribe(_ => {
+      $('ul.tabs').tabs();
+    })
+  }
 
   scrollToElement($elem): void {
     $elem.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
