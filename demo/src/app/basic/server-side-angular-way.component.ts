8007ded1f8fae5e6993457f97d39108579f48ed9
Louis Lin <lin.louis@pm.me>
2023-11-12 20:59:19 +0100

    chore: remove server side previews
    We no longer have any free server that can serve the HTTP queries for
    server-side processing.

--- demo/src/app/basic/server-side-angular-way.component.ts
+++ demo/src/app/basic/server-side-angular-way.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { Person } from '../person';
-import { DataTablesResponse } from 'app/datatables-response.model';
 
 @Component({
   selector: 'app-server-side-angular-way',
   templateUrl: 'server-side-angular-way.component.html',
   styleUrls: ['server-side-angular-way.component.css']
 })
-export class ServerSideAngularWayComponent implements OnInit {
+export class ServerSideAngularWayComponent {
 
   pageTitle = 'Server side the Angular way';
   mdIntro = 'assets/docs/basic/server-side-angular-way/intro.md';
@@ -17,33 +15,4 @@ export class ServerSideAngularWayComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {};
   persons: Person[];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    const that = this;
-
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2,
-      serverSide: true,
-      processing: true,
-      ajax: (dataTablesParameters: any, callback) => {
-        that.http
-          .post<DataTablesResponse>(
-            'https://xtlncifojk.eu07.qoddiapp.com/',
-            dataTablesParameters, {}
-          ).subscribe(resp => {
-            that.persons = resp.data;
-
-            callback({
-              recordsTotal: resp.recordsTotal,
-              recordsFiltered: resp.recordsFiltered,
-              data: []
-            });
-          });
-      },
-      columns: [{ data: 'id' }, { data: 'firstName' }, { data: 'lastName' }]
-    };
-  }
 }
